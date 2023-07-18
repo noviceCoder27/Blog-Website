016dad013abfb9310122c338e4951ca3b4948701
mdGoswami <u19cse1001@cit.ac.in>
2023-07-18 21:11:36 +0530

    create user routes for the server

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/regiser',() => {});
+router.post('/login',() => {});
+router.put('/updateCredentials',() => {});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const app = express();
+const userRoutes = require('./routes/userRoutes');
+
+
+app.use('/',userRoutes);
 
 app.listen(3000, () => {
     console.log("Listening to port 3000");
