e4ed80d4f0ab9ce4bf7eaf17008b0541bd53aa3f
mdGoswami <u19cse1001@cit.ac.in>
2023-07-19 01:16:34 +0530

    create users mongo schema

---
+++ backend/models/usersModel.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+
+
+const Schema = mongoose.Schema();
+
+const usersSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String
+    },
+    userDescription: {
+        type: String
+    },
+    
+},{timestamps: true});
+
+usersSchema.statics.register = async function(email,password,userName,userDescription){
+    if(!email || !password) {
+        throw Error("Please fill in the required credentials");
+    }
+    if(!validator(isEmail)) {
+        throw Error("Enter a valid email");
+    }
+
+    const doesExist = await this.findOne({email});
+    if(doesExist) {
+        throw Error("Email already exists");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password,salt);
+    const user = await this.create({email,password:hash,userName,userDescription});
+    return user;
+}
+
+usersSchema.statics.login = async function(email,password){
+    if(!email || !password) {
+        throw Error("Please fill in the required credentials");
+    }
+    if(!validator(isEmail)) {
+        throw Error("Enter a valid email");
+    }
+
+    const user = await this.findOne({email});
+    if(!user) {
+        throw Error("Email doesn't exists");
+    }
+    
+    const isValidPasswaord = await bcrypt.compare(password,user.password);
+    if(!isValidPasswaord) {
+        throw Error("Invalid Password");
+    }
+    return user;
+}
+
+// usersSchema.statics.updateCredentials = async function(email,password,userName,userDescription) {
+//     if(!email || !password) {
+//         throw Error("Please fill in the required credentials");
+//     }
+//     if(!validator(isEmail)) {
+//         throw Error("Enter a valid email");
+//     }
+
+//     const user = await this.findOne({email});
+//     if(!user) {
+//         throw Error("User doesn't exists");
+//     }
+
+
+
+// }
+
+
+module.exports = mongoose.model("Users", usersSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
-
+const mongoose = require('mongoose');
 
 app.use('/user',userRoutes);
 
-app.listen(3000, () => {
-    console.log("Listening to port 3000");
-});
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("Listening to port " + process.env.PORT);
+    });
+})
+.catch((err) => {
+    console.log("ERROR connecting to mongoose "+ err.message);
+})
