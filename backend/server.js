b3c15b6fa7759e8286cad76b7f936f1c57bdf028
mdGoswami <u19cse1001@cit.ac.in>
2023-07-23 01:13:35 +0530

    fix: cors origin to connect to frontend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173/"
+    origin: "http://localhost:5173"
 }));
 app.use('/user',userRoutes);
 app.use('/blogs',blogRoutes);
