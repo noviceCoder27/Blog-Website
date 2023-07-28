bfd052195d45d7262a69d116b0f86701f2229408
mdGoswami <u19cse1001@cit.ac.in>
2023-07-28 20:40:55 +0530

    feat: get user blogs(not working)

--- frontend/src/pages/UserBlogs.jsx
+++ frontend/src/pages/UserBlogs.jsx
@@ -1,5 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react"
 
 export const UserBlogs = () => {
+  useEffect(() => {
+    async function getUserBlogs() {
+      try {
+        const userBlogs = await axios.get("http://localhost:3000/me");
+        console.log(userBlogs);
+      } catch(err) {
+        console.log(err);
+      }  
+    }
+    getUserBlogs();
+  },[]);
   return (
     <div>UserBlogs</div>
   )
