6e057cba2dc790c114e6d0ee7253b7b26f855ef8
mdGoswami <u19cse1001@cit.ac.in>
2023-07-29 00:31:03 +0530

    code to get blog content(auth token not implemented)

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom"
+import axios from 'axios'
 
 export const Blog = () => {
+  const {id} = useParams();
+  useEffect(() => {
+    async function getBlogContent() {
+      const content = await axios.get(`http://localhost:3000/blogs/${id}`);
+    }
+    getBlogContent();
+  })
   return (
     <div>Blog</div>
   )
--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,22 +1,30 @@
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 import axios from "axios"
-
+import {useNavigate} from 'react-router-dom'
 
 export const Blogs = () => {
+    const [blogs,setBlogs] = useState([]);
     useEffect(() => {
         async function getBlogs() {
             try {
-                const blogs = await axios.get("http://localhost:3000/blogs");
-                console.log(blogs);
+                const blogsObj = await axios.get("http://localhost:3000/blogs");
+                const getBlogs = blogsObj?.data;
+                setBlogs(getBlogs)
             } catch(err) {
                 console.log(err);
             }
         }
         getBlogs();
     },[])
+    const navigate = useNavigate();
     return (
         <div>
-
+            {blogs?.map(blog => (
+                <div key = {blog._id}>
+                    <p>{blog.title}</p>
+                    <button onClick={() => navigate(`/blogs/${blog._id}`)}>Check Blog</button>
+                </div>
+            ))}
         </div>
     )
 }
