8cb7ca0f4babeffe796d23d6279252e60462cf74
mdGoswami <u19cse1001@cit.ac.in>
2023-07-31 12:04:19 +0530

    chore: add selectors for blog

---
+++ frontend/src/store/selectors/blogs.js
@@ -0,0 +1,34 @@
+import { selector } from "recoil";
+import { blogState } from './../atoms/blogAtom';
+
+export const getBlogTitle = selector({
+    key: "getBlogTitle",
+    get: ({get}) => {
+        const state = get(blogState);
+        return state?.title;
+    }
+});
+
+export const getBlogContent = selector({
+    key: "getBlogContent",
+    get: ({get}) => {
+        const state = get(blogState);
+        return state?.content;
+    }
+});
+
+export const getBlogCategory = selector({
+    key: "getBlogCategory",
+    get: ({get}) => {
+        const state = get(blogState);
+        return state?.category;
+    }
+});
+
+export const getBlogCreatedAt = selector({
+    key: "getBlogCreatedAt",
+    get: ({get}) => {
+        const state = get(blogState);
+        return state?.createdAt;
+    }
+});
