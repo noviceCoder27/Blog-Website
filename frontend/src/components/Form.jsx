6633a5738fd3dd499d76129ed7994cd97e6b1b95
mdGoswami <u19cse1001@cit.ac.in>
2023-08-12 18:03:18 +0530

    fix: input type text to password

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -10,7 +10,7 @@ export const Form = ({email,setEmail,password,setPassword,signin}) => {
             <label htmlFor="email" className="mt-5 translate-x-[-12vw] font-monsterrat font-bold">Email :</label>
             <input type = "text" name = "email" value = {email} onChange = {(e) => {setEmail(e.target.value)}} className="text-center min-w-[30vw] bg-[#eeeeee] p-2 py-3 border-black border-4 rounded-[50px] font-monsterrat text-lg font-bold mb-4 lg:flex-1" placeholder="Enter your email"/>
             <label htmlFor="password" className="translate-x-[-11.5vw] font-monsterrat font-bold" >Password: </label>
-            <input type = "text" name = "password" value = {password} onChange = {(e) => {setPassword(e.target.value)}} className="text-center min-w-[30vw] bg-[#eeeeee] p-2 py-3 border-black border-4 rounded-[50px] font-monsterrat text-lg font-bold mb-4 lg:flex-1" placeholder="Enter your password"/>
+            <input type = "password" name = "password" value = {password} onChange = {(e) => {setPassword(e.target.value)}} className="text-center min-w-[30vw] bg-[#eeeeee] p-2 py-3 border-black border-4 rounded-[50px] font-monsterrat text-lg font-bold mb-4 lg:flex-1" placeholder="Enter your password"/>
             <button onClick={(e) => signin(e)} className="p-2 py-3 px-10 rounded-[50px] border-4 border-black font-monsterrat font-extrabold text-lg bg-[#ffcc00] hover:bg-gray-300 ">Send</button>
         </form>
     )
